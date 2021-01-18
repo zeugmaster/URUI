cbc21006cecaa27bd9fee7b8a40478b457baa0b5
Wolf McNally <wolf@wolfmcnally.com>
2021-01-18 04:18:56 -0800

    Added ability to make QR code as UIImage.

--- Sources/URUI/URQRCode.swift
+++ Sources/URUI/URQRCode.swift
@@ -50,19 +50,31 @@ public enum QRCorrectionLevel: String {
 }
 
 public func makeQRCode(_ message: Data, correctionLevel: QRCorrectionLevel = .medium) -> Image {
+    return Image(uiImage: makeQRCodeImage(message, correctionLevel: correctionLevel))
+        .renderingMode(.template)
+        .interpolation(.none)
+}
+
+public func makeQRCodeImage(_ message: Data, correctionLevel: QRCorrectionLevel = .medium, foregroundColor: UIColor = .black, backgroundColor: UIColor = .clear) -> UIImage {
     let qrCodeGenerator = CIFilter.qrCodeGenerator()
     qrCodeGenerator.correctionLevel = correctionLevel.rawValue
     qrCodeGenerator.message = message
 
     let falseColor = CIFilter.falseColor()
     falseColor.inputImage = qrCodeGenerator.outputImage
-    falseColor.color0 = .black
-    falseColor.color1 = .clear
+    falseColor.color0 = foregroundColor.ciColorValue
+    falseColor.color1 = backgroundColor.ciColorValue
 
     let output = falseColor.outputImage!
 
     let cgImage = CIContext().createCGImage(output, from: output.extent)!
-    return Image(uiImage: UIImage(cgImage: cgImage, scale: 1, orientation: .up))
-        .renderingMode(.template)
-        .interpolation(.none)
+    return UIImage(cgImage: cgImage, scale: 1, orientation: .up)
+}
+
+extension UIColor {
+    var ciColorValue: CIColor {
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return CIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
 }
