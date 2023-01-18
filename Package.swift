35900b555d4d01014c46dc2e779fe23db3ee8e10
Wolf McNally <wolf@wolfmcnally.com>
2023-01-18 00:53:10 -0800

    5.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["URUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "8.0.0"),
+        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "9.0.0"),
     ],
     targets: [
         .target(
