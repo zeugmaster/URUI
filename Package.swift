ff26f9e771e541a9b3ab3a48fd538491a0bf5234
Wolf McNally <wolf@wolfmcnally.com>
2023-04-28 02:22:03 -0700

    Fixed dependency.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["URUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "11.0.0"),
+        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "11.2.1"),
     ],
     targets: [
         .target(
